7bad0d17c68d35de6fbb693797cba1b7eca8670a
entmike <mike.howles@gmail.com>
2018-07-13 23:09:39 +0000

    Sample app folder

---
+++ app/scripts/startServe.js
@@ -0,0 +1,42 @@
+const FaceServer = require("entmike-facetest"),
+AWS = require("aws-sdk"),
+AWS2 = require("aws-sdk");
+
+process.on('SIGINT', function() {
+    process.exit();
+});
+AWS.config.update({
+	region: "us-west-2"
+});
+
+AWS2.config.update({
+	region: "us-west-2",
+	credentials : new AWS.SharedIniFileCredentials({profile: 'personal-account'})
+});
+
+FaceServer.listen({
+	servers : {
+		// Example HTTP server.  AWS account and S3 bucket name needed.
+		4321 : {
+			AWS : AWS,
+			bucket : "com.entmike.miketest1"
+		},
+		// Example running a second AWS account
+		4322 : {
+			AWS : AWS2,
+			bucket : "com.entmike.miketest2"
+		},
+		// Example running in https.  Certs required to work.
+		4421 : {
+			AWS : AWS,
+			bucket : "com.entmike.miketest2",
+			ssl : {
+				certs : {
+					key : "certs/private.pem",
+					ca :"certs/ca.pem",
+					cert : "certs/cert.pem"
+				}
+			}
+		}
+	}
+});
